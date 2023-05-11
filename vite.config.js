0f9ceb8f8f2627f1dbeb07ea367ad04f2f272ea7
steven-kid <zyin0002@student.monash.edu>
2023-05-11 16:41:32 +0800

    update vite.config.js, setup pages configuration

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,15 @@
-import { defineConfig } from 'vite';
-
+import { defineConfig } from "vite"
+import path from 'path'
 export default defineConfig({
-  // 指定项目的根目录
-  root: './',
-  // 指定输出目录
   build: {
-    outDir: 'dist'
-  },
-  // 配置多页应用
-  server: {
-    // 指定默认打开的页面
-    index: '/index.html',
-    // 配置多个页面
-    cors: true,
-    fs: {
-      strict: false,
-    },
-    // 配置多个页面
-    pages: {
-      index: {
-        // 模板文件
-        template: 'public/index.html',
-        // 输出文件名
-        filename: 'index.html',
-        // 页面标题
-        title: 'Home Page'
-      },
-      team: {
-        template: 'public/team.html',
-        filename: 'team.html',
-        title: 'Our Team'
-      },
-      contactus: {
-        template: 'public/contactus.html',
-        filename: 'contactus.html',
-        title: 'Contact Us'
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, './index.html'),
+        team: path.resolve(__dirname, './team.html'),
+        project: path.resolve(__dirname, './project.html'),
+        contactus:path.resolve(__dirname, './contactus.html'),
+        news:path.resolve(__dirname, './news.html')
       }
     }
   }
-});
+})
