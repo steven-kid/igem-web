730db1edec858de7edfedda6d44ce12f8200db02
steven-kid <zyin0002@student.monash.edu>
2023-05-11 16:50:17 +0800

    fix configure bug

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
       input: {
         index: path.resolve(__dirname, './index.html'),
         team: path.resolve(__dirname, './team.html'),
-        project: path.resolve(__dirname, './project.html'),
+        project: path.resolve(__dirname, './projects.html'),
         contactus:path.resolve(__dirname, './contactus.html'),
         news:path.resolve(__dirname, './news.html')
       }
